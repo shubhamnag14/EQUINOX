5abcdc247154ad3d18d0a23292b4064a7584f212
shubhamnag14 <43213463+shubhamnag14@users.noreply.github.com>
2018-11-06 18:05:43 +0530

    Add files via upload

---
+++ Bootstrap_ecommerce_websdesign_templet-master/index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>E-shop</title>
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link type="text/css" rel="stylesheet" href="css/font-awesome.min.css"/>
+  <link type="text/css" rel="stylesheet" href="main.css"/>
+</head>
+<body>
+    
+         <nav class="navbar navbar-default">
+          <div class="container-fluid">
+             <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html">Brand</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                
+                <li><a href="#">HOME</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+            </div>
+          </div>
+         </nav>
+         <!--navbar end-->
+
+         <!--banner carosel start-->
+         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img class="banner" src="img/banner-1.jpg" alt="banner-1">
+                <div class="carousel-caption">
+                  <img class="banner-title pull-left img-responsive" src="img/banner_title.png" alt="banner_title">
+                </div>
+              </div>
+              <div class="item">
+                 <img class="banner"  src="img/banner-2.jpg" alt="banner-2">
+                <div class="carousel-caption">
+                  <img class="banner-title center-block img-responsive" src="img/banner_title.png" alt="banner_title">
+                </div>
+              </div>
+               <div class="item">
+                <img class="banner" src="img/banner.jpg" alt="banner-3">
+                <div class="carousel-caption">
+                  <img class="banner-title pull-left img-responsive" src="img/banner_title.png" alt="banner_title">
+                </div>
+              </div>
+              <div class="item">
+                 <img class="banner"  src="img/banner-2.jpg" alt="banner-4">
+                <div class="carousel-caption">
+                  <img class="banner-title center-block img-responsive" src="img/banner_title.png" alt="banner_title">
+                </div>
+              </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+          <!--banner carosel start-->
+
+          <!--some spacing-->
+          <div class="clearfix" style="margin-bottom:20px;"></div>
+          <!--some spacing-->
+
+
+         <!--banner end-->
+         <div class="marketing text-center">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-4 col-md-4">
+                <img class="img-circle" alt="marketing-1" src="img/marketing-1.png">
+                <h2>Titele here</h2>
+                <p>developmentHello every one tthid one of the most wanderful site
+                 where you can learn eveything about the web design and 
+                 web developmentHello every one tthid one of the most wanderful</p>
+              </div>
+              <div class="col-xs-12 col-sm-4 col-md-4">
+                <img class="img-circle" alt="marketing-2" src="img/marketing-2.png">
+                <h2>Titele here</h2>
+                <p>developmentHello every one tthid one of the most wanderful site
+                 where you can learn eveything about the web design and 
+                 web developmentHello every one tthid one of the most wanderful</p>
+              </div>
+              <div class="col-xs-12 col-sm-4 col-md-4">
+                <img class="img-circle" alt="marketing-3" src="img/marketing-3.png">
+                <h2>Titele here</h2>
+                <p>developmentHello every one tthid one of the most wanderful site
+                 where you can learn eveything about the web design and 
+                 web developmentHello every one tthid one of the most wanderful</p>
+              </div>
+            </div>
+          </div>
+         </div>
+         <!--marketing end-->
+
+        <!--product start-->
+          <div class="product text-center">
+            <div class="container">
+                 <div class="row">
+                <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-1.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                             <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-2.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                             <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-3.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                             <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-4.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+                                 <div class="row">
+                <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-5.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                             <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-6.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                             <div class="  col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-1.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                             <div class=" col-sm-6 col-md-3">
+                  <div class="thumbnail">
+                    <img class="product-img img-responsive center-block" src="img/product-8.png" alt="product-1">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>span span span</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+            </div>
+          </div>
+         <!--product end-->
+
+        <!--footer start-->
+        <div class="footer">
+          <div class="container">
+            <p class="pull-right"><a href="#">Back to Top</a></p>
+            <p class="pull-left">Designed By Imran .<a href="">Privacy</a> . <a href="">Terms</a></p>
+          </div>
+        </div>
+        <!--footer end-->
+
+
+
+
+	<script src="js/jquery-1.11.3.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+
+
+</body>
+</html>
